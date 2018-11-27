36183e12b4c8ff240868b281bbb86afe00fc9fbc
Jason Draper <jason@drapergeek.com>
2018-11-27 09:53:06 -0500

    Use a little less `true` and `false`
    It's a little difficult to follow along with the boolean-checking when there are so many booleans. Instead of `true`, use `_` as the fallback in the cond.
    
    Co-Authored-By: dbernheisel <dbernheisel@users.noreply.github.com>

--- lib/formulator/input.ex
+++ lib/formulator/input.ex
@@ -47,7 +47,7 @@ defmodule Formulator.Input do
       Keyword.get(options, field) == false -> false
       Keyword.get(options, field) == true -> true
       Application.get_env(:formulator, field, default) == true -> true
-      true -> false
+      _ -> false
     end
   end
 
