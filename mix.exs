52d21d608a05e6cfbe7e7a9f27d9f05d9028b294
Kian-Meng, Ang <kianmeng@cpan.org>
2020-11-11 19:27:54 +0800

    Misc doc changes
    List of changes:
    
    - Mix format module config
    - Remove duplicated doc link
    - Use common source url
    - Link function header to github
    - Switch relative links to absolute links

--- mix.exs
+++ mix.exs
@@ -1,24 +1,21 @@
 defmodule Formulator.Mixfile do
   use Mix.Project
+
+  @source_url "https://github.com/thoughtbot/formulator"
   @version "0.2.0"
 
   def project do
-    [app: :formulator,
-     version: @version,
-     elixir: "~> 1.6",
-     elixirc_paths: elixirc_paths(Mix.env),
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps(),
-     docs: [
-       source_ref: "#{@version}",
-       extras: [
-         "README.md",
-       ],
-       main: "readme"
-     ],
-     package: package(),
-   ]
+    [
+      app: :formulator,
+      version: @version,
+      elixir: "~> 1.6",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      docs: docs(),
+      package: package()
+    ]
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
@@ -30,11 +27,20 @@ defmodule Formulator.Mixfile do
 
   defp deps do
     [
+      {:gettext, ">= 0.11.0"},
+      {:phoenix_html, "~> 2.4"},
       {:ecto, "~> 2.1", only: :test, optional: true},
       {:ex_doc, "~> 0.18.0", only: :dev, runtime: false},
-      {:gettext, ">= 0.11.0"},
-      {:phoenix_ecto, "~> 3.2", only: :test, optional: true},
-      {:phoenix_html, "~> 2.4"},
+      {:phoenix_ecto, "~> 3.2", only: :test, optional: true}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      source_url: @source_url,
+      source_ref: "#{@version}",
+      extras: ["README.md"]
     ]
   end
 
@@ -44,10 +50,9 @@ defmodule Formulator.Mixfile do
       maintainers: ["Jason Draper", "Ashley Ellis", "Josh Steiner"],
       licenses: ["MIT"],
       links: %{
-        "Docs" => "https://hexdocs.pm/formulator/",
-        "GitHub" => "https://github.com/thoughtbot/formulator",
-        "Made by thoughtbot" => "https://thoughtbot.com/services/elixir-phoenix",
-      },
+        "GitHub" => @source_url,
+        "Made by thoughtbot" => "https://thoughtbot.com/services/elixir-phoenix"
+      }
     ]
   end
 end
